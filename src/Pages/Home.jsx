d37d24a5363e4fe9b120dfddf08190adfa3a69e4
apoerakay <apoerak@gmail.com>
2024-03-19 16:28:04 +0000

    tailwind installed

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -4,11 +4,10 @@ import React from "react"
 export default function Home(){
     return(
         <>
-    <div className="inset-0 bg-[url('./assets/bg2.jpg')]  p-8 bg-center bg-cover h-lvh text-center text-white">
-        <div>
-
-        </div>
-    </div>
+   <h1 class="text-3xl font-bold underline">
+    Hello world!
+  </h1>
+    
         </>
     )
 };
