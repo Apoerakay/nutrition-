ef21fc3ddd8a7351d0cd6d0ad7098c01a84f8fa9
apoerakay <apoerak@gmail.com>
2024-03-20 04:47:45 +0000

    work on landing page

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,38 @@
 import React from "react"
+import Navbar from "../Components/Navbar"
 
 
-export default function Home(){
-    return(
+export default function Home() {
+    return (
         <>
-   <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-    
+            <div className="bg-[url('https://media.istockphoto.com/id/1478737872/photo/apple-safety-nutritionist.jpg?s=1024x1024&w=is&k=20&c=4wHHsi0JcW8y0fqp2Zx-UdKm7VVGaZd45lHMK5Q2SnA=.jpg') h-lvh]">
+
+                <Navbar />
+                <div className="flex justify-center items-center h-screen">
+                    <button type="button" class="text-gray-900 hover:text-white border border-gray-800 hover:bg-gray-900 focus:ring-4 focus:outline-none focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 mb-2 dark:border-gray-600 dark:text-gray-400 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-800 ">Dark</button>
+                </div>
+            </div>
+            
+
+<section class="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
+    <div class="px-4 mx-auto max-w-screen-xl text-center py-24 lg:py-56">
+        <h1 class="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">We invest in the world’s potential</h1>
+        <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Here at Flowbite we focus on markets where technology, innovation, and capital can unlock long-term value and drive economic growth.</p>
+        <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
+            <a href="#" class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
+                Get started
+                <svg class="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                </svg>
+            </a>
+            <a href="#" class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 sm:ms-4 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
+                Learn more
+            </a>  
+        </div>
+    </div>
+</section>
+
+
         </>
     )
 };
